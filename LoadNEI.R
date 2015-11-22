d1bb1fa52cac5adac02597ba943b23898c6ec0cf
Cal <cal@tripleanalytics.com>
2015-11-22 14:46:13 -0500

    Initial commit

---
+++ LoadNEI.R
@@ -0,0 +1,5 @@
+## Script to load NEI data from EPA for processing
+
+NEI <- readRDS("summarySCC_PM25.rds") # National Emissions Invetory
+SCC <- readRDS("Source_Classification_Code.rds") # Classification codes for SCC col in NEI
+
