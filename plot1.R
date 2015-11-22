1c430a6f1d4f434242a9f3fde46b369fc752f018
Cal <cal@tripleanalytics.com>
2015-11-22 16:06:03 -0500

    Plots 1 & 2 complete
    Working on 3...

--- plot1.R
+++ plot1.R
@@ -1,3 +1,5 @@
+## Script to investigate total PM2.5 emissions in the US over 9 years
+
 ## Script to load NEI data from EPA for processing
 library(data.table)
 library(dplyr)
@@ -16,12 +18,23 @@ by_year <- NEI %>% group_by(year)
 year_sum <- by_year %>% summarize(total = sum(Emissions))
 # Create point-plot using base system
 year_sum$total <-round(year_sum$total/1000000,2) # Scale by millions
+
 plot(year_sum,
      pch = 15,
      xlab = "Year",
      ylab = "Total Emissions (Million Tons)")
-lines(year_sum,
+
+lines(year_sum, # Include line to emphasize trend..
       lty = 2,
       lwd = 2,
       col = "Blue")
 title("US PM2.5 Emissions Decreasing 1999-2008")
+
+# Prepare plot for copy to png
+dev.copy(png, "plot1.png")
+
+# Inspect plot & confirm save to png
+cat ("Press [enter] to save plot")
+readline()
+dev.off()
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,42 @@
+## Script to investigate specific PM2.5 emissions in Baltimore over 9 years
+# Looking at types: point, nonpoint, onroad, nonroad
+
+## Script to load NEI data from EPA for processing
+library(data.table)
+library(dplyr)
+
+# Don't re-load data if already in memory...
+if(!exists("NEI") || (exists("NEI")&nrow(NEI) != 6497651))
+{
+  NEI <- readRDS("summarySCC_PM25.rds") # National Emissions Invetory
+  SCC <- readRDS("Source_Classification_Code.rds") # Classification codes for SCC col in NEI
+}
+
+## Plot total emissions from baltimore 1999-2008
+
+NEI_baltimore <- NEI %>% filter(fips == 24510)
+# Group data by year & type
+by_year <- NEI_baltimore %>% group_by(year, type)
+year_sum <- by_year %>% summarize(total = sum(Emissions))
+# Create point-plot using base system
+year_sum$total <-round(year_sum$total,2)
+
+plot(year_sum,
+     pch = 15,
+     xlab = "Year",
+     ylab = "Total Emissions (Thousand Tons)")
+
+lines(year_sum, # Include line to emphasize trend..
+      lty = 2,
+      lwd = 2,
+      col = "Blue")
+title("Baltimore PM2.5 Emissions 1999-2008")
+
+# Prepare plot for copy to png
+dev.copy(png, "plot2.png")
+
+# Inspect plot & confirm save to png
+cat ("Press [enter] to save plot")
+readline()
+dev.off()
+dev.off()
